32ac8043b73b75b2c13571384cfeb759ba457b23
Alex J Garcia <alexshuu@gmail.com>
2022-01-27 20:35:26 -0500

    Set up basic grid and rudimentary logic

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 "use strict";
 
 (function() {
-  
+  let dragArea = document.querySelector(".dnd-container");
+  dragArea.addEventListener("mousedown", startDrag);
+  // if the element is of the type we're looking for, add a mousemove event
+  // listener to it (this MUST be removed on mouseup otherwise it could have
+  // too many listeners attached).
+  // The mousemove event's .toElement property indicates where the drag is
+  // hovering over.
+  let isDragging = false;
+  function startDrag({target}) {
+    let classes = Array.from(target.classList);
+    if (classes.includes("dnd-item")) {
+      isDragging = true;
+      dragArea.addEventListener("mousemove", setMove);
+      dragArea.addEventListener("mouseup", onMouseUp);
+    }
+  }
+
+  function setMove(event) {
+    if (isDragging) {
+      console.log(event);
+    }
+  }
+
+  function onMouseUp() {
+    dragArea.removeEventListener("mousemove", setMove);
+    isDragging = false;
+  }
 })();
